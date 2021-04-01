fef29b4fd6f81b6aba17341b9b7636df5b88a68d
Anthony Bodenhorst <anthony.bodenhorst@ulb.be>
2021-04-01 07:35:30 +0200

    [ADD] end traitement dataSet

--- commun/datasets/ANN/traitetementDatasetANN.py
+++ commun/datasets/ANN/traitetementDatasetANN.py
@@ -1,1 +1,47 @@
-import pan
+import pandas as pd
+import numpy as np
+
+
+
+class CSVTraitement:
+
+    def __init__(self,csvToLoad):
+        self.df = pd.read_csv(csvToLoad)
+
+    def deletColumn(self,lstColumn):
+        self.df = self.df.drop(lstColumn, axis=1)
+
+    def getData(self):
+        return self.df
+
+    def oneHotEncoder(self,column):
+        tmp = pd.get_dummies(self.df[column],drop_first=True) #drop first pour enlevé une colonne
+        indexCol = self.df.columns.get_loc(column)
+        self.df = self.concatenateColumn(self.concatenateColumn(self.df.iloc[:,:indexCol],tmp),self.df.iloc[:,indexCol+1:])
+
+
+    def concatenateColumn(self,elemOne,elemTwo):
+        return pd.concat([elemOne,elemTwo],axis=1)
+
+
+    def normalize(self,column):
+        self.df[column] = self.df[column] / self.df[column].abs().max()
+
+    def saveData(self,path):
+        self.df.to_csv(path,index=False)
+
+
+data = CSVTraitement("Churn_Modelling.csv")
+data.deletColumn(["RowNumber","CustomerId","Surname"])
+
+data.oneHotEncoder("Geography")
+data.oneHotEncoder("Gender")
+
+data.normalize("CreditScore")
+data.normalize("Age")
+data.normalize("Tenure")
+data.normalize("Balance")
+data.normalize("NumOfProducts")
+data.normalize("EstimatedSalary")
+
+data.saveData('Churn_Modelling_traited.csv')
