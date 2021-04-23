4657addf13281ece4c0410fe1d85dae3c2da6fe6
Anthony Bodenhorst <anthony.bodenhorst@ulb.be>
2021-04-24 00:40:06 +0200

    [ADD] ANN keras

--- commun/CommunFonction.py
+++ commun/CommunFonction.py
@@ -1,24 +1,9 @@
-import os
+def writeIntoFiles(lstValTest, lstValTrain,time, name):
 
-def comptageTot(folderPathTrain, folderPathTest):
-    """ compte le nombre total de photo """
-    imageTest = 0
-    imageTrain = 0
-    foldersTest = os.listdir(folderPathTest)
-    foldersTrain = os.listdir(folderPathTrain)
-    for subFolders in foldersTrain:
-        imageTrain += comptageImage(folderPathTrain + "/" + subFolders)
-    for subFolders in foldersTest:
-        imageTest += comptageImage(folderPathTest + "/" + subFolders)
-    return (imageTrain, imageTest)
-
-
-def comptageImage(pathFolder):
-    folder = os.listdir(pathFolder)
-    return len(folder)
-
-def writeIntoFile(lstValTest, lstValTrain, name):
-    f = open("./{name}.txt".format(name=name), "a")
+    y = open("Timer.txt","a")
+    y.write("{name} : {time} \n".format(name=name,time=time))
+    y.close()
+    f = open("./{name}.txt".format(name=name), "w")
     toWrite = "Epoch,ACC_Test,ACC_Train \n"
     f.write(toWrite)
     for i in range(len(lstValTest)):
