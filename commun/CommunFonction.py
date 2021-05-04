5a6b1cce3252e52838ead6b95c9ebff3667f689b
Anthony Bodenhorst <anthony.bodenhorst@ulb.be>
2021-05-04 02:47:22 +0200

    [Refactoring] keras

--- commun/CommunFonction.py
+++ commun/CommunFonction.py
@@ -1,15 +1,14 @@
-def writeIntoFiles(lstValTest, lstValTrain,time, name):
-
+def writeIntoFilesKeras(history,time, name):
     y = open("Timer.txt","a")
     y.write("{name} : {time} \n".format(name=name,time=time))
     y.close()
     f = open("./{name}.txt".format(name=name), "w")
     toWrite = "Epoch,ACC_Test,ACC_Train \n"
     f.write(toWrite)
-    for i in range(len(lstValTest)):
-        precisionTest = lstValTest[i]
-        precisionEntrainement = lstValTrain[i]
-        toWrite = str(i+1) +","+str(round(precisionTest,4))+ ","+str(round(precisionEntrainement,4))+" \n"
+    for i in range(len(history.history['val_acc'])):
+        toWrite = "{epoch},{ACC_test},{ACC_train}".format(epoch = i+1,ACC_train = _arrondi(history.history['acc'][i]),ACC_test = _arrondi(history.history['val_acc'][i]))
         f.write(toWrite)
     f.close()
 
+def _arrondi(number):
+    return round(number,4)
