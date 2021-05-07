090480afb06d2dc6dcfe0b5b2848931f932c8459
Anthony Bodenhorst <anthony.bodenhorst@ulb.be>
2021-05-07 03:59:13 +0200

    [UPDATE] fonction commun

--- commun/CommunFonction.py
+++ commun/CommunFonction.py
@@ -1,14 +1,15 @@
-def writeIntoFilesKeras(history,time, name):
+def writeIntoFilesKeras(lstVal,lstValACC,lstLOSS,lstLOSSACC,time, name):
     y = open("Timer.txt","a")
     y.write("{name} : {time} \n".format(name=name,time=time))
     y.close()
     f = open("./{name}.txt".format(name=name), "w")
-    toWrite = "Epoch,ACC_Test,ACC_Train \n"
+    toWrite = "Epoch,ACC_Train,LOSS_Train,ACC_Test,LOSS_test \n"
     f.write(toWrite)
-    for i in range(len(history.history['val_acc'])):
-        toWrite = "{epoch},{ACC_test},{ACC_train}".format(epoch = i+1,ACC_train = _arrondi(history.history['acc'][i]),ACC_test = _arrondi(history.history['val_acc'][i]))
+    for i in range(len(lstVal)):
+        toWrite = "{epoch},{ACC_train},{LOSS_train},{ACC_test},{LOSS_test} \n".format(epoch = i+1, ACC_train = arrondi(lstVal[i]), ACC_test = arrondi(lstValACC[i]),
+                                                                                      LOSS_test = arrondi(lstLOSSACC[i]), LOSS_train= arrondi(lstLOSS[i]))
         f.write(toWrite)
     f.close()
 
-def _arrondi(number):
+def arrondi(number):
     return round(number,4)
